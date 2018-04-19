90b7844f83c4549beb1906328025d4866e106538
Jason Herning <jmherning@alaska.edu>
2018-04-19 10:00:45 -0800

    added gui for game board, still rough

---
+++ guiBoard.hpp
@@ -0,0 +1,35 @@
+#ifndef GUIBOARD_HPP_INCLUDED
+#define GUIBOARD_HPP_INCLUDED
+
+
+
+#include <SFML/Graphics.hpp>
+
+
+class guiBoard : public sf::Drawable, public sf::Transformable
+{
+public:
+
+    bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height);
+
+private:
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        // apply the transform
+        states.transform *= getTransform();
+
+        // apply the tileset texture
+        states.texture = &m_tileset;
+
+        // draw the vertex array
+        target.draw(m_vertices, states);
+    }
+
+    sf::VertexArray m_vertices;
+    sf::Texture m_tileset;
+
+};
+
+
+#endif // GUIBOARD_HPP_INCLUDED
